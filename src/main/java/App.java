9ee61c7eeadd6f0e2a7ffc05be1e197eb69d069c
Ilya <arzaevilya@gmail.com>
2019-07-19 02:40:34 +0700

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    JFrame frame = new JFrame("Hello World Swing!");
+                    frame.setSize(500, 400);
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setVisible(true);
+                }
+        });
+    }
+}
